c6335cf80bda9922402ac110fe58f5fc0795a9d5
Carlos Baraza Haro <carlos.baraza@ieee.org>
2021-02-09 01:05:42 +0000

    Improving Sizing and Scaling page

--- pages/docs/sizing-system.tsx
+++ pages/docs/sizing-system.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import React from "react";
+import { Code } from "../../components/docs/Code";
 import { Heading1 } from "../../components/docs/Heading1";
 import { Heading2 } from "../../components/docs/Heading2";
 import { Layout } from "../../components/docs/Layout";
@@ -17,24 +18,43 @@ const DocsSizingSystem = (props: Props) => {
         </Head>
         <Layout>
           <Section>
+            <Heading2>Why do I need a Sizing System?</Heading2>
+            <p>
+              A <strong>sizing system</strong> is a great tool to have when
+              building user interfaces because it{" "}
+              <strong>limits the choices</strong> for a padding, a width, a
+              margin, etc. It will help you avoid playing the pixel game. Every
+              size in the scale is clearly differentiated from its closer sizes,
+              and that makes it extremely easy to select the right size.
+            </p>
+            <p>
+              If you stick to the scaling system, all your UIs will have a{" "}
+              <strong>good vertical and horizontal spacing rhythm</strong>.
+              Every small detail makes a huge difference in the look and feel of
+              your interface.
+            </p>
+          </Section>
+          <Section>
             <Heading1>Sizing and Scaling System</Heading1>
             <SizingSystem />
           </Section>
           <Section>
-            <Heading2>Why do I need a Sizing System?</Heading2>
+            <Heading1>Customizing</Heading1>
             <p>
-              A sizing system is a great tool to have when building user
-              interfaces because it limits the possibilities when choosing the
-              size for a padding, a width, a margin, etc. It will help you avoid
-              playing the pixel game. Every size in the scale is clearly
-              differentiated from its closer sizes, and that makes it extremely
-              easy to select the right size.
+              Dashvar's sizing and spacing system should have enough choices for
+              most interfaces. However, you can always extend it with your own
+              sizes.
             </p>
             <p>
-              If you stick to the scaling system, all your UIs will have a good
-              vertical and horizontal spacing rhythm. Every small detail makes a
-              huge difference in the good and feel of an interface.
+              You can add new sizes adding more variables to the{" "}
+              <code>:root</code> pseudo-class:
             </p>
+            <Code language="css">{`:root {
+  --size-sm: 10px; 
+  --size-m: 20px; 
+  --size-l: 40px; 
+  --size-xl: 80px; 
+}`}</Code>
           </Section>
         </Layout>
       </div>
